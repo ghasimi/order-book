4ddf1327e79173af34c718013a4568fe1f95c883
ghasimi <25824939+ghasimi@users.noreply.github.com>
2026-04-18 13:30:45 -0400

    Initiate with orderbook downloader

---
+++ data.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from pathlib import Path
+
+
+def download_raw_orderbook(sym: str, date: str):
+	"""Downloads the Order Book zip file.
+
+	Sample filename:
+	"2026-04-04_BTCUSDT_ob200.data.zip"
+	
+	:param sym: 'BTCUSDT'
+	:param date: 'yyyy-mm-dd'
+	:return: tuple of result (True/False) and 
+		msg (path/to/file if True, error message otherwise)
+	"""
+	try:
+		# 1. URL
+		url = f'https://quote-saver.bycsi.com/orderbook/spot/{sym}/{date}_{sym}_ob200.data.zip'
+		
+		# 2. Landing directory
+		dir = f'blob/raw/orderbook/{sym}/{date}'
+		Path(dir).mkdir(parents=True, exist_ok=True)
+
+		# 3. Path
+		fname = url.split('/')[-1]
+		fpath = os.path.join(dir, fname)
+
+		# 4. Download (Streaming)
+		chunk_size = 2**13 # ~8KB
+		with requests.get(url, stream=True) as res:
+			res.raise_for_status()
+			with open(fpath, 'wb') as f:
+				for chunk in res.iter_content(chunk_size=chunk_size):
+					_ = f.write(chunk)
+
+		return True, fpath
+	
+	except Exception as err:
+		return False, err
+	
