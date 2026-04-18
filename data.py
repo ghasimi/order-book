53de3b9da8abc17b86540c537d7d4c209076323a
ghasimi <25824939+ghasimi@users.noreply.github.com>
2026-04-18 14:20:18 -0400

    Create directory only on successful response

--- data.py
+++ data.py
@@ -20,7 +20,6 @@ def download_raw_orderbook(sym: str, date: str):
 		
 		# 2. Landing directory
 		dir = f'blob/raw/orderbook/{sym}/{date}'
-		Path(dir).mkdir(parents=True, exist_ok=True)
 
 		# 3. Path
 		fname = url.split('/')[-1]
@@ -30,6 +29,11 @@ def download_raw_orderbook(sym: str, date: str):
 		chunk_size = 2**13 # ~8KB
 		with requests.get(url, stream=True) as res:
 			res.raise_for_status()
+
+			# 5. Create directory
+			Path(dir).mkdir(parents=True, exist_ok=True)
+
+			# 6. Save
 			with open(fpath, 'wb') as f:
 				for chunk in res.iter_content(chunk_size=chunk_size):
 					_ = f.write(chunk)
